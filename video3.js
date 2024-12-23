3a38a64f38a549d05a219da14c9fdb29f52afe1c
Shu1237 <ttei8191@gmail.com>
2024-12-23 10:16:51 +0700

    first commit

---
+++ video3.js
@@ -0,0 +1,46 @@
+
+let c = 'jone';
+let d = 5;
+let e= 8;
+
+let a = `I'am a string`
+let text = `we are the so-called "Vikkings" from the north              `;
+text.trim;
+const str= 'the quick brown for jumps over the lazy dog';
+const word= str.split(' ');
+
+
+let h='5';
+// convert string => number
+// c1 
+// let k=Number(h);
+// c2 
+let q=+h;
+// =>subsenquently comparison with other numebers
+
+
+console.log(typeof h, typeof q);
+console.log(h===q)
+
+
+
+
+
+// console.log(typeof(word));
+// console.log(word.length)
+// console.log(word[5],word[6]);
+
+// console.log(str.toUpperCase());
+// let x = 5, y = 6, z = 7;
+
+
+// console.log('test string:', 'x=', x, 'y=', y, 'z=', z);
+
+// console.log(text.length);
+
+// // plus left to right
+// console.log(c + x + y); //jone56
+// console.log(x + y + c);  //11jone
+
+
+
